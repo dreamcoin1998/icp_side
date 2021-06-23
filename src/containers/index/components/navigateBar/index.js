bec19fdcd3e8ff865519d17526dc7d5077e1f5fb
gaojunbin <15259695263@163.com>
2021-06-24 02:18:38 +0800

    Update 导航栏列表组件

---
+++ src/containers/index/components/navigateBar/index.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Link, BrowserRouter, withRouter } from 'react-router-dom';
+import { Menu } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
+
+
+const { SubMenu, Item } = Menu;
+
+class NavigateBar extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            productTypes: [{
+                id: 1,
+                typeName: "高端权益"
+            },{
+                id: 2,
+                typeName: "视听娱乐"
+            },{
+                id: 3,
+                typeName: "美食餐饮"
+            },{
+                id: 4,
+                typeName: "生活服务"
+            },{
+                id: 5,
+                typeName: "旅游出行"
+            },{
+                id: 6,
+                typeName: "高端权益"
+            },{
+                id: 7,
+                typeName: "医护健康"
+            },{
+                id: 8,
+                typeName: "高端权益"
+            },]
+        }
+    }
+
+    componentDidMount() {
+        // TODO: 调用获取产品分类列表接口
+
+    }
+
+    handleSelect(data) {
+        const { key } = data;
+        // TODO: 调用获取产品分类列表接口
+
+    }
+
+    render() {
+        // 产品类型的Item列表数组
+        const ProductTypeItem = this.state.productTypes.map((productType) => (
+            <Item key={String(productType.id)}>{productType.typeName}</Item>
+        ))
+
+        return (
+            <Menu 
+              mode="inline" 
+              defaultOpenKeys={["productTypes"]} 
+              defaultSelectedKeys={["productTypes"]}
+              onSelect={this.handleSelect}
+            >
+                <SubMenu 
+                  icon={ <MenuOutlined /> } 
+                  title="全部产品分类" 
+                  key="productTypes"
+                  children={ProductTypeItem}
+                >
+                </SubMenu>
+            </Menu>
+        );
+    }
+
+}
+
+
+export {
+    NavigateBar
+};
--- src/containers/index/index.js
+++ src/containers/index/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout } from 'antd';
 import { TopBar } from '../../components/header';
+import { NavigateBar } from './components/navigateBar';
 
 
 const { Header, Footer, Sider, Content } = Layout;
@@ -9,16 +10,18 @@ const { Header, Footer, Sider, Content } = Layout;
 class IndexPage extends React.Component {
     render() {
         return (
-            <>
+            <div>
                 <Layout>
                     <TopBar></TopBar>
-                    {/* <Layout>
-                        <Sider>Sider</Sider>
-                        <Content>Content</Content>
+                    <Layout style={{marginTop: "1em"}}>
+                        <Sider>
+                            <NavigateBar></NavigateBar>
+                        </Sider>
+                        {/* <Content>Content</Content> */}
                     </Layout>
-                    <Footer>Footer</Footer> */}
+                    {/* <Footer>Footer</Footer> */}
                 </Layout>
-            </>
+            </div>
         )
     }
 }
