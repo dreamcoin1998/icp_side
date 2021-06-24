3c82e30d771ed0f6610f9602a20c25b5b0e6c789
gaojunbin <15259695263@163.com>
2021-06-25 01:05:15 +0800

    点击卡片跳转 && 分页处理逻辑函数

--- src/containers/index/components/displayContainer/index.js
+++ src/containers/index/components/displayContainer/index.js
@@ -9,15 +9,13 @@ const { Content } = Layout;
 
 class DisplayContainer extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            productInfo: {
-                images: ["https://cdn.xiangshuheika.com/static/xs_oak/spu/trainviproom.png"],
-                price: 4000.00,
-                product_name: "凯越酒店凯越酒店凯越酒店凯越酒店凯越酒店凯越酒店",
-                update_time: "2021-06-24 11:50"
-            }
+    /**
+     * 跳转到产品详情页
+     * @param id 产品id
+     */
+    handleClick(id) {
+        if (id) {
+            this.props.history.push("/product/detail/" + id)
         }
     }
     
@@ -26,17 +24,22 @@ class DisplayContainer extends React.Component {
         // 产品列表的Item列表数组
         const ProductsItem = this.props.products.map((product) => (
             <Col span={5}>
-                {/* TODO: 添加点击卡卡片跳转事件 */}
-                <ProductCard data={ product } />
+                <ProductCard data={ product } onClick={this.handleClick.bind(this, product.id)}/>
             </Col>
         ))
 
         return (
             <Content className="container">
                 <Row style={{paddingTop: "1.4em"}} justify="space-around" wrap={false}>
-                    <ProductsItem />
+                    {ProductsItem}
                 </Row>
-                <Pagination className="pagenation-padding" defaultCurrent={1} total={50} />
+                <Pagination 
+                    className="pagenation-padding" 
+                    defaultCurrent={1} 
+                    total={this.props.total} 
+                    defaultPageSize={4}
+                    onChange={this.props.paginationChange}
+                />
             </Content>
         );
     }
--- src/containers/index/components/productTypied/index.js
+++ src/containers/index/components/productTypied/index.js
@@ -16,31 +16,70 @@ class ProductTypied extends React.Component {
             // 产品类型
             productTypes: [],
             // 产品分类/列表
-            products: []
+            products: [],
+            // 产品列表产品总数
+            productListTotal: 0,
+            // 导航栏点击状态
+            navigatorKey: "productTypes"
         };
+    }
+
+    getProductList(count, page) {
+        // 调用产品列表接口
+        const productListResponse = request('get', '/v1.0/products?count=' + count + '&page=' + page);
+        const productListData = productListResponse.data;
+        const productListTotal = productListResponse.total;
+        return productListData, productListTotal;
+    }
+
+    getProductTypied(key, count, page) {
+         // 调用获取产品分类列表接口
+         const productTypiedResponse = request('get', "/v1.0/products/type?id=" + key + "&count=" + count + "&page=" + page)
+         const productTypiedData = productTypiedResponse.data
+         return productTypiedData
     }
 
     componentDidMount() {
         // 调用获取产品类型列表接口
-        const productTypeResponse = request('get', '/v1.0/product_types/')
-        const productTypeData = productTypeResponse.data
-        // 调用产品列表接口
-        const productListResponse = request('get', '/v1.0/products?count=4&page=1')
-        const productListData = productListResponse.data
+        const productTypeResponse = request('get', '/v1.0/product_types/');
+        const productTypeData = productTypeResponse.data;
+        console.log(this.getProductList(4, 1))
+        const {productListData, productListTotal} = this.getProductList(4, 1);
         this.setState({
             productTypes: productTypeData,
-            products: productListData
+            products: productListData,
+            productListTotal: productListTotal
+
         })
     }
 
     handleSelect(data) {
         const { key } = data;
-        // 调用获取产品分类列表接口
-        const productTypiedResponse = request('get', "/v1.0/products/type?id=" + key + "&count=&page=")
-        const productTypiedData = productTypiedResponse.data
-        this.setState({
-            products: productTypiedData
-        })
+        if (key !== "productTypes") {
+            const productTypiedData = this.getProductTypied(key, 4, 1);
+            this.setState({
+                navigatorKey: key,
+                products: productTypiedData
+            })
+        }
+    }
+    
+    handlePaginationChange(page, _) {
+        // 用户点击全部产品信息
+        if (this.state.navigatorKey === "productTypes") {
+            const {productListData, productListTotal} = this.getProductList(4, page);
+            this.setState({
+                products: productListData,
+                productListTotal: productListTotal
+    
+            });
+        // 用户点击产品分类
+        } else {
+            const productTypiedData = this.getProductTypied(this.state.navigatorKey, 4, page);
+            this.setState({
+                products: productTypiedData
+            })
+        }
     }
 
     render() {
@@ -52,7 +91,11 @@ class ProductTypied extends React.Component {
                         handleSelect={this.handleSelect.bind(this)}
                     />
                 </Sider>
-                <DisplayContainer products={this.state.products}/>
+                <DisplayContainer 
+                    products={this.state.products} 
+                    total={this.state.productListTotal}
+                    paginationChange={this.handlePaginationChange.bind(this)}
+                />
             </Layout>
         );
     }
