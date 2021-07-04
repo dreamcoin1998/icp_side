6058fcee35724d4534a38a462e929894c50bbc50
gaojunbin <15259695263@163.com>
2021-07-05 07:50:22 +0800

    部署

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -6,6 +6,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const friendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -17,14 +20,48 @@ module.exports = {
     publicPath: '/'
   },
   plugins: [
+    new UglifyJsPlugin(),
     new HtmlWebpackPlugin({
       title: '',
       template: path.resolve(__dirname, '../public/index.html'),
       filename: 'index.html',
+      minify: {// 压缩HTML文件
+        removeComments: true,//去除注释
+        collapseWhitespace: true,//去除空格
+      },
     }),
     new CleanWebpackPlugin(),
     new friendlyErrorsWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[hash].css',
+      chunkFilename: 'css/[id].[hash].css',
+    }),
   ],
+  optimization: {
+    minimizer: [ // 最小化器
+      new TerserPlugin({// 缩减代码的插件
+        terserOptions: {
+          compress: {
+            drop_console: true, // 删除console.log
+          },
+        }
+      })
+    ],
+    splitChunks: {// 抽离公共代码 具体配置看官网
+      chunks: 'all',// 效值是all、async和initial。提供all可能特别强大，因为这意味着即使在异步和非异步块之间也可以共享块
+      minSize: 30000,
+      maxSize: 100000,
+      minChunks: 1,
+      cacheGroups: {// 定义了被抽离的模块如何分成组，不然公共代码全打包到一个JS文件里面
+        vendors: {// 第三方库抽离
+          priority: 1,// 权重 先进行第三方库抽离
+          test:  /[\\/]node_modules[\\/]/,// 选从node_modules文件夹下引入的模块，所以所有第三方模块才会被拆分出来 递归的
+          name: "vendor",
+          enforce: true,
+        },
+      }
+    }
+  },
   module: {
     rules: [
       // JavaScript
@@ -39,20 +76,21 @@ module.exports = {
         },
       },
       {
-        test: /\.(scss|css)$/,
-        use: ['style-loader', {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-            },
-          }, 'postcss-loader'],
+        test: /\.(scss|sass)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
       },
       {
         test: /\.css$/,
-        include: [
-          '/src/',
-        ],
-        use: ['style-loader', 'css-loader'],
+        use: [ 
+          MiniCssExtractPlugin.loader,// 单独提出CSS
+          'css-loader',
+          'postcss-loader'// 对css编译的工具 可以：1.使用下一代css语法 2 . 自动补全浏览器前缀 3 . 自动把px代为转换成rem 
+        ]
       },
       {
         test: /\.tsx?$/,
@@ -72,7 +110,8 @@ module.exports = {
       // CSS, PostCSS, and Sass
       {
         test: /\.less$/,
-        use: [{
+        use: [
+        {
           loader: 'style-loader',
         }, {
           loader: 'css-loader', // translates CSS into CommonJS
